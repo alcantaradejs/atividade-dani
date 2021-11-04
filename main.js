f16e6ec794bb4609cea6c1ccd2cbc07712892fbd
BatatinhasFritas <alcantara.isaque167@gmail.com>
2021-11-04 09:17:27 -0300

    versao de entrega

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ import { confChargeGraph } from './controler/chartGraph.js';
 function loadGraph(Vp,shootingAngleSCR1, shootingAngleSCR2, angleRange) {
     const chargeGraphChart = new Chart(
         document.getElementById('chargeGraph'),
-        confChargeGraph(chargeGraph(Vp,shootingAngleSCR1, shootingAngleSCR2, angleRange), "senoide da carga")
+        confChargeGraph(chargeGraph(Vp,shootingAngleSCR1, shootingAngleSCR2, angleRange), "tensão na carga resistiva")
     );
 
     const scr1GraphChart = new Chart(
         document.getElementById('scr1Graph'),
-        confChargeGraph(SCR1Graph(Vp,shootingAngleSCR1, shootingAngleSCR2, angleRange), "senoide da SCR1")
+        confChargeGraph(SCR1Graph(Vp,shootingAngleSCR1, shootingAngleSCR2, angleRange), "tenção no SCR1")
     );
 
     const scr2GraphChart = new Chart(
         document.getElementById('scr2Graph'),
-        confChargeGraph(SCR2Graph(Vp,shootingAngleSCR1, shootingAngleSCR2, angleRange), "senoide da SCR2")
+        confChargeGraph(SCR2Graph(Vp,shootingAngleSCR1, shootingAngleSCR2, angleRange), "tensão no SCR2")
     );
 }
 
-const Vp = 110
+const Vp = 155.56
 const anguloDisparoSCR1 = 30
-const anguloDisparoSCR2 = 30
+const anguloDisparoSCR2 = 10
 
 loadGraph(Vp, anguloDisparoSCR1, anguloDisparoSCR2, 1)
